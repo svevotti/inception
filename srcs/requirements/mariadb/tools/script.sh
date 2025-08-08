ecaa21397866dd894c4bc2fe85e23fef25f5a41f
Sveva Mazzari <smazzari88@gmail.com>
2025-08-08 22:41:20 +0200

    updata password reading in script mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -7,11 +7,14 @@ mysql_install_db --user=mysql --datadir=/var/lib/mysql
 DOCKER_INIT_DIR=/etc/mysql/docker-entrypoint-initdb.d
 INIT_SQL_PATH=$DOCKER_INIT_DIR/init.sql
 
+#reading password from file
+USER_PASS=$(cat $MYSQL_PASSWORD)
+
 mkdir $DOCKER_INIT_DIR
 # chmod -R 777 $INIT_SQL_PATH
 echo "CREATE DATABASE $MYSQL_DATABASE;" > $INIT_SQL_PATH
 echo "USE $MYSQL_DATABASE;" >> $INIT_SQL_PATH
-echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $INIT_SQL_PATH
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$USER_PASS';" >> $INIT_SQL_PATH
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> $INIT_SQL_PATH
 echo "FLUSH PRIVILEGES;" >> $INIT_SQL_PATH
 
